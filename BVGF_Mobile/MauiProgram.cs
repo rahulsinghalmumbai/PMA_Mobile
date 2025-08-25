111f07a5a12b115d5680eac656ecaf4ea25cfd36
rahul <rahulsinghalmumbai@gmail.com>
2025-08-25 11:31:22 +0530

    Add project files.

---
+++ BVGF_Mobile/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using BVGF.Connection;
+using BVGF.Pages;
+using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Media;
+using Microsoft.Extensions.Logging;
+
+namespace BVGF_Mobile
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit() 
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcons");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<HttpClient>();
+            builder.Services.AddSingleton<ApiService>();
+            builder.Services.AddSingleton<ISpeechToText>(SpeechToText.Default);
+            builder.Services.AddTransient<homePage>();
+
+
+            return builder.Build();
+        }
+    }
+}
